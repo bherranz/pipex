fecc106e5a37f06cb688db677c28b6c11c4a5a27
Belen Herranz Campusano <bherranz@c1r16s2.42madrid.com>
2024-05-13 15:35:45 +0200

    looks like it works

--- main.c
+++ main.c
@@ -30,12 +30,37 @@ void	process_in(char *file, char *cmd, char **envp, int *in_pipe)
 	else if (pid == 0)
 	{
 		fd = open(file, O_RDONLY);
-		
 		if (fd < 0)
 			print_error("Error while opening the file");
 		close(in_pipe[0]);
 		dup2(fd, STDIN_FILENO);
-		//dup2(in_pipe[1], STDOUT_FILENO);
+		close(fd);
+		dup2(in_pipe[1], STDOUT_FILENO);
+		close(in_pipe[1]);
+		execute(cmd, envp);
+	}
+	else
+		waitpid(pid, &status, 0);
+}
+
+void	process_out(char *file, char *cmd, char **envp, int *in_pipe)
+{
+	pid_t	pid;
+	int		fd;
+	int		status;
+
+	pid = fork();
+	if (pid < 0)
+		print_error("Error forking");
+	else if (pid == 0)
+	{
+		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd < 0)
+			print_error("Error while opening the file");
+		close(in_pipe[1]);
+		dup2(fd, STDOUT_FILENO); 
+		close(fd);
+		dup2(in_pipe[0], STDIN_FILENO);
 		execute(cmd, envp);
 	}
 	else
@@ -45,30 +70,17 @@ void	process_in(char *file, char *cmd, char **envp, int *in_pipe)
 		waitpid(pid, &status, 0);
 	}
 }
-/*
-int	process_out(char *file)
-{
-
-}*/
 
 int	main(int argc, char **argv, char **envp)
 {
 	int		my_pipe[2];
 
 	if (argc != 5)
-	{
-		perror("Incorrect format");
-		exit (1);
-	}
+		print_error("Incorrect format");
 	if (pipe(my_pipe) < 0)
 		print_error("Error creating the pipe");
 	process_in(argv[1], argv[2], envp, my_pipe);
-
-	//process_out(argv[3], argv[4], envp);
+	process_out(argv[4], argv[3], envp, my_pipe);
+	close(my_pipe[0]);
+	close(my_pipe[1]);
 }
-
-/*
-Steps:
-	Encontrar path: buscar path en envp, despues buscar en esas carpetas
-	y si no encuentras el comando, lo mandas sin path(el comando tal cual)
-*/
